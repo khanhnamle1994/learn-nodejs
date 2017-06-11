bf9f30de5288517b0b8a7b307d73f9fd9c502616
James Le <le_j6@denison.edu>
2017-06-10 20:39:03 -0700

    Let's Build A Module

--- app.js
+++ app.js
@@ -1,25 +1,2 @@
-// function statement
-function greet() {
-  console.log('hi');
-}
+var greet = require('./greet');
 greet();
-
-// functions are first-class
-function logGreeting(fn) {
-  fn();
-}
-logGreeting(greet);
-
-// function expression
-var greetme = function() {
-  console.log('Hi Tony');
-}
-greetme();
-
-// it's first-class
-logGreeting(greetme);
-
-// use a function expression on the fly
-logGreeting(function() {
-  console.log('Hello Tony!');
-});
