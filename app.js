667cd70bd38b2fbb0c5e2cdc060e321dd1b353c0
James Le <le_j6@denison.edu>
2017-06-12 16:27:13 -0700

    Node Event Emitter Part I

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
-// object properties and methods
-var obj = {
-  greet: 'Hello'
-}
+var Emitter = require('./emitter');
 
-console.log(obj.greet);
-console.log(obj['greet']);
-var prop = 'greet';
-console.log(obj[prop]);
+var emtr = new Emitter();
 
-// functions and arrays
-var arr = []
-
-arr.push(function() {
-  console.log('Hello world 1');
-});
-arr.push(function() {
-  console.log('Hello world 2');
-});
-arr.push(function() {
-  console.log('Hello world 3');
+emtr.on('greet', function() {
+  console.log('My name is James');
 });
 
-arr.forEach(function(item) {
-  item();
+emtr.on('greet', function() {
+  console.log('Storyville Coffee');
 });
+
+console.log('Yo');
+emtr.emit('greet');
