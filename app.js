f167b7e37771399688599da2e098cf6d00db792c
James Le <le_j6@denison.edu>
2017-06-12 20:43:34 -0700

    Node, ES6, and Template Literals

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
-var EventEmitter = require('events');
-var util = require('util');
+var name = 'James Le';
 
-function Greetr() {
-  this.greeting = 'Hello world!';
-}
+var greet = 'Hello ' + name;
+var greet2 = `Hello ${ name }`;
 
-util.inherits(Greetr, EventEmitter);
-
-Greetr.prototype.greet = function(data) {
-  console.log(this.greeting + ': ' + data);
-  this.emit('greet', data);
-}
-
-var greeter1 = new Greetr();
-
-greeter1.on('greet', function(data) {
-  console.log('Someone greeted!: ' + data);
-});
-
-greeter1.greet('James');
+console.log(greet);
+console.log(greet2);
