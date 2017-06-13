76ba590e2c3f9c682f5a56f6385d899cc7f6c416
James Le <le_j6@denison.edu>
2017-06-12 20:38:39 -0700

    Inhering From Event Emitter

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-var person = {
-  firstname: '',
-  lastname: '',
-  greet: function() {
-    return this.firstname + ' ' + this.lastname;
-  }
+var EventEmitter = require('events');
+var util = require('util');
+
+function Greetr() {
+  this.greeting = 'Hello world!';
 }
 
-var john = Object.create(person);
-john.firstname = 'John';
-john.lastname = 'Doe';
+util.inherits(Greetr, EventEmitter);
 
-var jane = Object.create(person);
-jane.firstname = 'Jane';
-jane.lastname = 'Doe';
+Greetr.prototype.greet = function(data) {
+  console.log(this.greeting + ': ' + data);
+  this.emit('greet', data);
+}
 
-console.log(john.greet());
-console.log(jane.greet());
+var greeter1 = new Greetr();
+
+greeter1.on('greet', function(data) {
+  console.log('Someone greeted!: ' + data);
+});
+
+greeter1.greet('James');
