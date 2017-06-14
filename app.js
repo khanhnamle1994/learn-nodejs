718df63c6b6b102d3ea1005a3b7fe8970f85bdbb
James Le <le_j6@denison.edu>
2017-06-13 20:55:14 -0700

    ES6 Typed Arrays

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-var buf = new Buffer('Hello', 'utf8');
-
-console.log(buf);
-console.log(buf.toString());
-console.log(buf.toJSON());
-console.log(buf[2]);
-
-buf.write('wo');
-console.log(buf.toString());
+var buffer = new ArrayBuffer(8);
+var view = new Int32Array(buffer);
+view[0] = 5;
+view[1] = 15;
+view[2] = 30;
+console.log(view);
