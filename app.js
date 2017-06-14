fbceb5bf88653c943340234177f4548d70273541
James Le <le_j6@denison.edu>
2017-06-13 21:21:19 -0700

    Streams

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
 
-var greet = fs.readFileSync(__dirname + '/greet.txt', 'utf8');
-console.log(greet);
+var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 16 * 1024 });
 
-var greet2 = fs.readFile(__dirname + '/greet.txt', 'utf8', function(err, data) {
-  console.log(data);
+var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
+
+readable.on('data', function(chunk) {
+  console.log(chunk);
+  writable.write(chunk);
 });
-
-console.log('Done!');
