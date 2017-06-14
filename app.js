5cfd10dfab66d04e5a5b149d8b814f64157e0edc
James Le <le_j6@denison.edu>
2017-06-13 21:29:47 -0700

    Pipes

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var fs = require('fs');
+var zlib = require('zlib');
 
-var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 16 * 1024 });
+var readable = fs.createReadStream(__dirname + '/greet.txt');
 
 var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
 
-readable.on('data', function(chunk) {
-  console.log(chunk);
-  writable.write(chunk);
-});
+var compressed = fs.createWriteStream(__dirname + '/greetcopy.txt.gz');
+
+var gzip = zlib.createGzip();
+
+readable.pipe(writable);
+
+readable.pipe(gzip).pipe(compressed);
