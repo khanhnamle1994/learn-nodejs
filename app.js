7b08f0195f2e934e678c971fe2979f572bfeff7b
James Le <le_j6@denison.edu>
2017-06-14 21:21:26 -0700

    Streams and Performance

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var fs = require('fs');
 http.createServer(function(req, res) {
 
   res.writeHead(200, { 'Content-Type' : 'text/html' });
-  var html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
-  var message = 'Hello world...';
-  html = html.replace('{Message}', message);
-  res.end(html);
+  fs.createReadStream(__dirname + '/index.htm').pipe(res);
 
 }).listen(1337, '127.0.0.1');
