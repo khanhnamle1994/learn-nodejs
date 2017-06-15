6cf5a8982c841cf241b9c1423ac4f805fb692ecf
James Le <le_j6@denison.edu>
2017-06-14 21:27:08 -0700

    JSON

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ var fs = require('fs');
 
 http.createServer(function(req, res) {
 
-  res.writeHead(200, { 'Content-Type' : 'text/html' });
-  fs.createReadStream(__dirname + '/index.htm').pipe(res);
+  res.writeHead(200, { 'Content-Type' : 'application/json' });
+  var obj = {
+    firstname: 'James',
+    lastname: 'Le'
+  };
+  res.end(JSON.stringify(obj));
 
 }).listen(1337, '127.0.0.1');
