16b2dcf05a9d54b51a462eea6dc91b3bb32b7bda
James Le <le_j6@denison.edu>
2017-06-14 21:33:48 -0700

    Routing

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ var fs = require('fs');
 
 http.createServer(function(req, res) {
 
-  res.writeHead(200, { 'Content-Type' : 'application/json' });
-  var obj = {
-    firstname: 'James',
-    lastname: 'Le'
-  };
-  res.end(JSON.stringify(obj));
+  if (req.url === '/') {
+    fs.createReadStream(__dirname + '/index.htm').pipe(res);
+  }
+
+  else if (req.url === '/api') {
+    res.writeHead(200, { 'Content-Type' : 'application/json' });
+    var obj = {
+      firstname: 'James',
+      lastname: 'Le'
+    };
+    res.end(JSON.stringify(obj));
+  }
+
+  else {
+    res.writeHead(404);
+    res.end();
+  }
+
 
 }).listen(1337, '127.0.0.1');
