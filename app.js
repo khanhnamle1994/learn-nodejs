08f4c98d76d0dd4b93ddf96ad5fb4e329ff40964
James Le <le_j6@denison.edu>
2017-06-15 16:01:09 -0700

    init, nodemon, and package.json

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-var moment = require('moment');
-console.log(moment().format("ddd, hA"));
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+
+  if (req.url === '/') {
+     fs.createReadStream(__dirname + '/index.htm').pipe(res);
+  }
+
+  else if (req.url === '/api') {
+    res.writeHead(200, { 'Content-Type' : 'application/json' });
+    var obj = {
+      firstname: 'James',
+      lastname: 'Le'
+    };
+    res.end(JSON.stringify(obj));
+  }
+
+  else {
+    res.writeHead(404);
+    res.end();
+  }
+
+}).listen(1337, '127.0.0.1');
