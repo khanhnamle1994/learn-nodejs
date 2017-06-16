86a045b4dc58af922e880553836188f4f0be7f41
James Le <le_j6@denison.edu>
2017-06-15 21:14:10 -0700

    Templates and Template Engines

--- app.js
+++ app.js
@@ -5,17 +5,19 @@ var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
+app.set('view engine', 'ejs');
+
 app.use('/', function(req, res, next) {
   console.log('Request Url: ' + req.url);
   next();
 });
 
 app.get('/', function(req, res) {
-  res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello world!</h1></body></html>');
+  res.render('index');
 });
 
 app.get('/person/:id', function(req, res) {
-  res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
+  res.render('person', { ID: req.params.id });
 });
 
 app.get('/api', function(req, res) {
