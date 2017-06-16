10624dab6280b159288ff86b95af09f85b2d9d0e
James Le <le_j6@denison.edu>
2017-06-15 19:11:49 -0700

    Routes

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', function(req, res) {
   res.send('<html><head></head><body><h1>Hello world!</h1></body></html>');
 });
 
+app.get('/person/:id', function(req, res) {
+  res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
+});
+
 app.get('/api', function(req, res) {
   res.json({ firstname: 'James', lastname: 'Le'});
 });
