3830e14e226d6f388892102f5068005894a9b018
James Le <le_j6@denison.edu>
2017-06-17 16:55:43 -0700

    Working with The Full Stack

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var people = [
+	{
+		name: 'James Le'
+	},
+	{
+		name: 'Harsh Sahgal'
+	},
+	{
+		name: 'Hanna Saba'
+	}
+];
+
 app.set('view engine', 'ejs');
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-	res.render('index');
+	res.render('index', { serverPeople: people});
 });
 
 app.listen(port);
--- public/js/app.js
+++ public/js/app.js
@@ -4,17 +4,5 @@ angular.module('TestApp')
   .controller('MainController', ctrlFunc);
 
 function ctrlFunc() {
-  this.message = 'Hello';
-
-  this.people = [
-    {
-      name: 'James Le'
-    },
-    {
-      name: 'Harsh Sahgal'
-    },
-    {
-      name: 'Hanna Saba'
-    }
-  ];
+  this.people = clientPeople;
 }
