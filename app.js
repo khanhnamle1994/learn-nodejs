7c5c07d7e45dceed5159cd62090223c319222532
James Le <le_j6@denison.edu>
2017-06-17 02:21:43 -0700

    Node and MySQL

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
-
 var app = express();
+var mysql = require('mysql');
 
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
@@ -13,6 +13,21 @@ app.set('view engine', 'ejs');
 
 app.use('/', function(req, res, next) {
   console.log('Request Url: ' + req.url);
+
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "test",
+    password: "test",
+    database: "addressbook"
+  });
+
+  con.query('SELECT People.ID, Firstname, Lastname, Address FROM People INNER JOIN PersonAddresses ON People.ID = PersonAddresses.PersonID INNER JOIN Addresses ON PersonAddresses.AddressID = Addresses.ID',
+      function(err, rows) {
+        if(err) throw err;
+        console.log(rows[0].Firstname);
+      }
+  );
+
   next();
 });
 
