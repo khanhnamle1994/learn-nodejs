7a11316492835c93ff813b728c5f57eae4ff9ef6
James Le <le_j6@denison.edu>
2017-06-18 13:02:11 -0700

    Adding Seed Data

--- NodeToDo/app.js
+++ NodeToDo/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
+var setupController = require('./controllers/setupController');
 
 var port = process.env.PORT || 3000;
 
@@ -9,6 +10,7 @@ app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
-mongoose.connect(config.getDBConnectionString());
+mongoose.connect(config.getDbConnectionString());
+setupController(app);
 
 app.listen(port);
